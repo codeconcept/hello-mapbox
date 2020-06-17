cf3450a90181abe4fbadf4aa1bf6b294fb3c0111
codeconcept <contact@codeconcept.fr>
2020-06-17 11:16:10 +0200

    Rennes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// documentation: https://docs.mapbox.com/help/tutorials/tilequery-healthy-food-finder/
+
 mapboxgl.accessToken = keys.privateKey; // set the access token
+
+const rennesParcThabor = { lat: 48.114384, lng: -1.669494 };
+const delta = 0.09;
 
 var map = new mapboxgl.Map({
     container: 'map', // The container ID
     style: 'mapbox://styles/mapbox/light-v10', // The map style to use
-    center: [-105.0178157, 39.737925], // Starting position [lng, lat] is Denver
-    zoom: 12 // Starting zoom level
+    center: [rennesParcThabor.lng, rennesParcThabor.lat], // Starting position [lng, lat] is Rennes
+    zoom: 14 // Starting zoom level
 });
 
 map.on('load', function() {
@@ -13,8 +18,17 @@ map.on('load', function() {
       mapboxgl: mapboxgl, // Set the mapbox-gl instance
       zoom: 13, // Set the zoom level for geocoding results
       placeholder: "Enter an address or place name", // This placeholder text will display in the search bar
-      bbox: [-105.116, 39.679, -104.898, 39.837] // Set a bounding box around Denver
+      bbox: [rennesParcThabor.lng - delta, rennesParcThabor.lat - delta, rennesParcThabor.lng + delta , rennesParcThabor.lat + delta] // Set a bounding box around Rennes
     });
     // Add the geocoder to the map
     map.addControl(geocoder, 'top-left'); // Add the search box to the top left
+
+    var marker = new mapboxgl.Marker({'color': '#008000'}) // Create a new green marker
+    
+    geocoder.on('result', function(data) { // When the geocoder returns a result
+      var point = data.result.center; // Capture the result coordinates
+    
+      marker.setLngLat(point).addTo(map); // Add the marker to the map at the result coordinates
+    
+    });
   });  
